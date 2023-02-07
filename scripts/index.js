99e6e0fdbae0f76ab4f482996832a1bf697256ed
Baron <scorpiov2000@gmail.com>
2023-02-07 14:20:14 -0500

    'First' submission

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
 console.log({ placeholderQuestions });
+let guessBtn = document.getElementById("guessBtn")
+let passBtn = document.getElementById("passBtn")
+
+
+guessBtn.disabled = true
+passBtn.disabled = true
+
