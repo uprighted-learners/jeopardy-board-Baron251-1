6c243f48208dd4f9737a501077c4357b65a60a59
Baron <scorpiov2000@gmail.com>
2023-03-23 18:37:49 -0400

    First submission part2

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 // Do not change the import statement
+// tHIS STATEMENT IMPORTS THE EXPORTED FILE SO IT'S CONTENTS ARE ACCESSIBLE TO US.
+// tHIS MAES THE "placeHolderQuestions" act like a variable essenetially
 import placeholderQuestions from "./placeholder-questions.js";
+// Is an object who's contents are the files data
 console.log({ placeholderQuestions });
+console.log(placeholderQuestions[0]);
+
 let guessBtn = document.getElementById("guessBtn")
 let passBtn = document.getElementById("passBtn")
 
